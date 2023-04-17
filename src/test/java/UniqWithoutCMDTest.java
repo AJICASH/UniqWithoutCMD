7bc577b1d10da09e9d189163b1f12fa0309c4dc9
usachev alexander <usachevalex20053@gmail.com>
2023-04-17 17:32:37 +0300

    помргите

--- src/test/java/UniqWithoutCMDTest.java
+++ src/test/java/UniqWithoutCMDTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class UniqWithoutCMDTest {
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UniqWithoutCMDTest {
+    UniqWithoutCMD file;
+    @Test
+    void UniqWithoutCMDTest() throws IOException {
+        file = new UniqWithoutCMD("input/intest1","output/outtest1");
+        assertEquals(Arrays.asList("Hello!", "abc", "abc", "abcde", "arfgd", "arfga", "aeaea", "aaaaa", "Goodbye!", "Goodbye!"), file.infileread());
+    }
+    @Test
+    void taskSort() throws IOException {
+        file = new UniqWithoutCMD("input/intest1","output/outtest1");
+        assertEquals(Arrays.asList("Hello!", "abc", "abcde", "arfgd", "arfga", "aeaea", "aaaaa", "Goodbye!"), file.taskSort(file));
+    }
 }
