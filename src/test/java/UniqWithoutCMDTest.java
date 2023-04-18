e371de849074840eb3d9a9df32794567091f7e14
usachev alexander <usachevalex20053@gmail.com>
2023-04-18 19:31:54 +0300

    сделаны -i -u -c флаги, но без cmd пока что

--- src/test/java/UniqWithoutCMDTest.java
+++ src/test/java/UniqWithoutCMDTest.java
@@ -1,6 +1,8 @@
+import org.example.UniqWithoutCMD;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,9 +15,29 @@ public class UniqWithoutCMDTest {
         file = new UniqWithoutCMD("input/intest1","output/outtest1");
         assertEquals(Arrays.asList("Hello!", "abc", "abc", "abcde", "arfgd", "arfga", "aeaea", "aaaaa", "Goodbye!", "Goodbye!"), file.infileread());
     }
+    // К КАЖДОМУ ТЕСТУ НАДО МЕНЯТЬ ФЛАГ ВРУЧНУЮ В Uniq.java ПОТОМУ ЧТО Я ПОКА НЕ ПОНЯЛ КАК СДЕЛАТЬ ТАК ЧТОБ КОМАНДНАЯ СТРОКА ЗАРАБОТАЛА
     @Test
     void taskSort() throws IOException {
+        //при -i
         file = new UniqWithoutCMD("input/intest1","output/outtest1");
-        assertEquals(Arrays.asList("Hello!", "abc", "abcde", "arfgd", "arfga", "aeaea", "aaaaa", "Goodbye!"), file.taskSort(file));
+        assertEquals(Arrays.asList("HeLLo!", "ABC", "abcde", "arfgd", "arfga", "aeaea", "aaaaa", "Goodbye!", "aaAAAAaad", "ALLAH"), file.taskSort(file));
+    }
+    @Test
+    void taskSort2() throws IOException {
+        //при -i -u
+        file = new UniqWithoutCMD("input/intest1","output/outtest1");
+        assertEquals(Arrays.asList("abcde", "arfgd", "arfga", "aeaea", "aaaaa", "aaAAAAaad"), file.taskSort(file));
+    }
+    @Test
+    void taskSort3() throws IOException {
+        //при -c
+        file = new UniqWithoutCMD("input/intest1","output/outtest1");
+        assertEquals(Arrays.asList("Hello!", "HeLLo!", "abc", "ABC", "abcde", "arfgd", "arfga", "aeaea", "aaaaa", "2 Goodbye!", "aaAAAAaad", "2 ALLAH"), file.taskSort(file));
+    }
+    @Test
+    void taskSort4() throws IOException {
+        //при -i -c
+        file = new UniqWithoutCMD("input/intest1","output/outtest1");
+        assertEquals(Arrays.asList("2 HeLLo!", "2 ABC", "abcde", "arfgd", "arfga", "aeaea", "aaaaa", "2 Goodbye!", "aaAAAAaad", "2 ALLAH"), file.taskSort(file));
     }
 }
